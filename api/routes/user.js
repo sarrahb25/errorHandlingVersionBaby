8843e55ea4d8c0391e16bcfc8f3b2957520216e0
Sarra Hattab <sarra.hattab@klarna.com>
2021-08-18 18:40:18 +0200

    add user login and jwt signin

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -35,6 +36,41 @@ router.post("/signup", (req, res, next) => {
           }
         });
       }
+    });
+});
+
+router.post("/login", (req, res, next) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then((user) => {
+      if (user.length < 1) {
+        // 401 Unauthorised is safer than 404 not found since they can guess which exist and which doesn't
+        return res.status(401).json({ message: "Auth failed" });
+      }
+      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+        if (err) {
+          return res.status(401).json({ message: "Auth failed" });
+        }
+        if (result) {
+          const token = jwt.sign(
+            {
+              email: user[0].email,
+              id: user[0]._id,
+            },
+            process.env.JWT_KEY,
+            {
+              expiresIn: "1h",
+            }
+          );
+          return res.status(200).json({ message: "Auth successful", token });
+        }
+        // of we don't make it into the conditions above means the password is incorrect and again we pass a 401 instead of 404
+        res.status(401).json({ message: "Auth failed" });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
     });
 });
 
